2e1c6d233473826cce7f8b8c588fd98b0b6b4d5e
Sam Yao <yaosam.521@gmail.com>
2023-09-12 21:05:15 -0500

    Added Switch Step Controller and View

---
+++ UIExample/SwitchStepController.swift
@@ -0,0 +1,39 @@
+//
+//  SwitchStepController.swift
+//  UIExample
+//
+//  Created by Sam Yao on 9/12/23.
+//
+
+import UIKit
+
+class SwitchStepController: UIViewController {
+
+    @IBOutlet weak var numberStepper: UIStepper!
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    lazy var numberModel = {
+        return NumberModel.sharedInstance()
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.numberLabel.text = "\(Int(numberModel.getRandomInt()))"
+        self.numberStepper.value = Double(numberModel.getRandomInt())
+    }
+    
+    @IBAction func numberStepChange(_ sender: UIStepper) {
+        numberModel.setRandomInt(Int(sender.value))
+        self.numberLabel.text = "\(numberModel.getRandomInt())"
+    }
+    
+    @IBAction func dankModeSwitch(_ sender: UISwitch) {
+        if sender.isOn{
+            self.view.backgroundColor = UIColor.green
+        }else{
+            self.view.backgroundColor = UIColor.white
+        }
+    }
+}
